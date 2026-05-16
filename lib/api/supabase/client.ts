3810156093d36e823126028b69c39f1189f3fb75
maru1005 <ellaumbrid@gmail.com>
2026-05-16 22:54:33 +0900

    first commit

---
+++ lib/api/supabase/client.ts
@@ -0,0 +1,11 @@
+// lib/api/supabase/client.ts
+// ブラウザCookie自動送受信
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASSE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+}
